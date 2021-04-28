91441990cff4263e401b0bf7ef927aa879bda59d
IgorNichiporuk <76099622+ig-nichiporuk@users.noreply.github.com>
2021-04-28 14:45:44 +0300

    fix lot-page bugs

--- src/js/lot-page.js
+++ src/js/lot-page.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	else {
 		check_device = false
 	}
-	$('.js-photoswipe').jqPhotoSwipe({
+	/*$('.js-lot-zoom').jqPhotoSwipe({
 		closeEl:true,
 		captionEl: true,
 		fullscreenEl: true,
@@ -17,28 +17,47 @@ $(document).ready(function() {
 		clickToCloseNonZoomable: true,
 		tapToClose: true,
 		tapToToggleControls: true,
-		mainClass: 'product__gallery-watch',
 		closeOnScroll: false,
 		loop: true,
+
 		// getDoubleTapZoom : false,
 		// forceSingleGallery: true
+	});*/
+
+	$('.js-lot-zoom').lightGallery({
+		speed: 200,
+		preload: 3,
+		swipeThreshold: 10,
+		enableDrag:false,
+		thumbnail: false,
+		autoplayControls: false,
+		hash: false,
+		share: false,
+		rotate:false,
+		download: false,
+		zoom: false,
+		actualSize: false,
+
+		controls: check_device ? false : true,
 	});
-	$('.js-product__photo-for').on('init', function () {
-		$('.slick-dots').wrap("<div class='slider__controls'></div>");
-		$('.slick-prev').prependTo('.slider__controls');
-		$('.slick-next').appendTo('.slider__controls');
+
+	$('.js-lot-slider').on('breakpoint init', function () {
+		$('.js-lot-slider-controls').empty();
+		$('.slick-prev').prependTo('.js-lot-slider-controls');
+		$('.slick-dots').appendTo('.js-lot-slider-controls');
+		$('.slick-next').appendTo('.js-lot-slider-controls');
 	});
-	$('.js-product__photo-for').slick({
-		lazyLoad: 'ondemand',
+	$('.js-lot-slider').slick({
+		// lazyLoad: 'ondemand',
 		slidesToShow: 2,
 		slidesToScroll: 2,
 		arrows: check_device ? false : true,
 		dots: true,
 		fade: false,
-		// asNavFor: $(this).next('.js-product__photo-nav'),
 		autoplay: false,
 		swipe: check_device ? true : false,
 		speed: 300,
 		touchThreshold: 100,
 	});
+
 })
