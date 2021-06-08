9095ed807bcee1c917128dad1d8f621100635ced
IgorNichiporuk <76099622+ig-nichiporuk@users.noreply.github.com>
2021-06-08 15:10:42 +0300

    fix swipe table type-page

--- src/js/type-page.js
+++ src/js/type-page.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
+
 	$('.js-table-slider').each(function() {
 		$(this).on('init', function () {
-			$(this).closest('.table__slider').next().find('.js-table-slider-dots').empty();
-			$(this).find('.slick-prev').prependTo($(this).closest('.table__slider').next().find('.js-table-slider-dots'));
-			$(this).find('.slick-dots').appendTo($(this).closest('.table__slider').next().find('.js-table-slider-dots'));
-			$(this).find('.slick-next').appendTo($(this).closest('.table__slider').next().find('.js-table-slider-dots'));
+			$(this).closest('.type-page__table-wrap').next().find('.js-table-slider-dots').empty();
+			$(this).find('.slick-prev').prependTo($(this).closest('.type-page__table-wrap').next().find('.js-table-slider-dots'));
+			$(this).find('.slick-dots').appendTo($(this).closest('.type-page__table-wrap').next().find('.js-table-slider-dots'));
+			$(this).find('.slick-next').appendTo($(this).closest('.type-page__table-wrap').next().find('.js-table-slider-dots'));
 		});
 		$(this).slick({
 			slidesToShow: 1,
@@ -14,8 +15,20 @@ $(document).ready(function() {
 			fade: false,
 			autoplay: false,
 			swipe: false,
-			speed: 300,
+			speed: 250,
 			touchThreshold: 100,
 		});
 	});
+	$('.js-table-mob').each(function () {
+		$(this).swipe({
+			swipeLeft:function() {
+				$(this).find('.js-table-slider').slick('slickNext');
+			},
+			swipeRight:function() {
+				$(this).find('.js-table-slider').slick('slickPrev');
+			},
+			threshold: 40,
+		});
+	});
+
 })
